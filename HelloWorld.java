61a3d12929aadd3d4d89414faf984eb4db016540
poctestuser <neetha.renjith@gmail.com>
2016-01-14 10:52:50 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         //add comment here
-        for (int i=0;i<10;i++){
         	System.out.println("Hello, World!");
-        }
     }
 }
